8de35e4bdd9fa19daffa485700f3005ff0a84fad
Zack-Z23 <zack.zacharidis@gmail.com>
2026-05-21 23:23:41 -0500

    Added comments

--- facedetect.py
+++ facedetect.py
@@ -1,13 +1,17 @@
+#Import library
 import cv2
 
+#Face var
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
 cap = cv2.VideoCapture(0)
 
+#Loop
 while True:
-	ret, frame = cap.read()
 	
+	ret, frame = cap.read()
+
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	
 	faces = face_cascade.detectMultiScale(gray,  scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
